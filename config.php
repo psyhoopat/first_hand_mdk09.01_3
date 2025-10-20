2a69ccbf453da11538821dff96c78438abdac651
jurball <233148174+psyhoopat@users.noreply.github.com>
2025-10-20 14:54:34 +0300

    update readme; update config file

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php 
 
-$but1 = '<a href="/php3/index.php?t=1">Главная</a><br/>';
+$but1 = '<a href="/first_hand_mdk09.01_3/index.php?t=1">Главная</a><br/>';
 
-$but2 = '<a href="/php3/news.php?t=2">Новости</a><br/>';
-$but2_1 = '<ul class="submenu"><a href="/php3/hotnews.php?t=2"><li>Горячие новости</li></a><br/>';
-$but2_2 = '<a href="/php3/lastnews.php?t=2"><li>Последние новости</li></a></ul>';
+$but2 = '<a href="/first_hand_mdk09.01_3/news.php?t=2">Новости</a><br/>';
+$but2_1 = '<ul class="submenu"><a href="/first_hand_mdk09.01_3/hotnews.php?t=2"><li>Горячие новости</li></a><br/>';
+$but2_2 = '<a href="/first_hand_mdk09.01_3/lastnews.php?t=2"><li>Последние новости</li></a></ul>';
 
-$but3 = '<a href="/php3/articles.php?t=3">Статьи</a><br/>';
-$but3_1 = '<ul class="submenu"><a href="/php3/order.php?t=3"><li>Заказать статью</li></a><br/>';
-$but3_2 = '<a href="/php3/top10.php?t=3"><li>ТОП 10</li></a></ul>';
+$but3 = '<a href="/first_hand_mdk09.01_3/articles.php?t=3">Статьи</a><br/>';
+$but3_1 = '<ul class="submenu"><a href="/first_hand_mdk09.01_3/order.php?t=3"><li>Заказать статью</li></a><br/>';
+$but3_2 = '<a href="/first_hand_mdk09.01_3/top10.php?t=3"><li>ТОП 10</li></a></ul>';
 
-$but4 = '<a href="/php3/categories.php?t=4">Рубрики</a><br/>';
+$but4 = '<a href="/first_hand_mdk09.01_3/categories.php?t=4">Рубрики</a><br/>';
 
-$but5 = '<a href="/php3/partners.php?t=5">Партнерам</a>';
+$but5 = '<a href="/first_hand_mdk09.01_3/partners.php?t=5">Партнерам</a>';
 
 ?>
 
--- template/header.php
+++ template/header.php
@@ -15,9 +15,9 @@
     </header>
 
     <nav class="navbar">
-        <a href="/php3/">главная</a> - 
-        <a href="/php3/news.php?t=2">новости</a> - 
-        <a href="/php3/articles.php?t=3">статьи</a> - 
-        <a href="/php3/categories.php?t=4">рубрики</a> - 
-        <a href="/php3/partners.php?t=5">партнерам</a> - 
+        <a href="/first_hand_mdk09.01_3/">главная</a> - 
+        <a href="/first_hand_mdk09.01_3/news.php?t=2">новости</a> - 
+        <a href="/first_hand_mdk09.01_3/articles.php?t=3">статьи</a> - 
+        <a href="/first_hand_mdk09.01_3/categories.php?t=4">рубрики</a> - 
+        <a href="/first_hand_mdk09.01_3/partners.php?t=5">партнерам</a> - 
     </nav>
